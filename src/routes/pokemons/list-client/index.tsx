e1702f560475a53a4a18c057d633760c613f87d2
Hugo <hugo@boahstudio.com>
2023-05-22 01:24:58 -0300

    Client-List page

--- src/routes/pokemons/list-client/index.tsx
+++ src/routes/pokemons/list-client/index.tsx
@@ -1,13 +1,67 @@
-import { component$ } from '@builder.io/qwik';
+import { component$, useStore, useVisibleTask$ } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
 
-// import styles from "../../styles.css?inline";
+import { PokemonImage } from '~/components/pokemons/pokemon-image';
+import { getSmallPokemons } from '~/helpers/get-small-pokemons';
+import type { SmallPokemon } from '~/interfaces';
+
+
+interface PokemonPageState {
+  currentPage: number;
+  pokemons: SmallPokemon[];
+}
 
 export default component$(() => {
 
-  // useStylesScoped$(styles);
+  const pokemonState = useStore<PokemonPageState>({
+    currentPage: 0,
+    pokemons: [],
+  });
 
-  return <span>Hola Mundo - List Client</span>
+  // Solo el cliente
+  useVisibleTask$(async({ track }) => {
+    
+    track( () => pokemonState.currentPage );
+
+    const pokemons = await getSmallPokemons( pokemonState.currentPage * 10);
+    pokemonState.pokemons = [ ...pokemonState.pokemons, ...pokemons ];  
+    
+  });
+
+  return (
+    <>
+      <div class="flex flex-col">
+        <span class="my-5 text-5xl">Status</span>
+        <span>Página actual: { pokemonState.currentPage } </span>
+        <span>Está cargando: </span>
+      </div>
+
+      <div class="mt-10">
+        {/* <button 
+          onClick$={ () => pokemonState.currentPage-- }
+          class="btn btn-primary mr-2">
+          Anteriores        
+        </button> */}
+
+        <button 
+          onClick$={ () => pokemonState.currentPage++ }
+          class="btn btn-primary mr-2">
+          Siguiente        
+        </button>
+      </div>
+
+      <div class="grid grid-cols-6 mt-5">
+        {
+          pokemonState.pokemons.map( ({ name, id }) => (
+            <div key={ name } class="m-5 flex flex-col justify-center items-center">
+              <PokemonImage id={ id }/>
+              <span class="capitalize">{ name }</span>
+            </div>
+          ))
+        }
+      </div>
+    </>
+  )
 });
 
 
