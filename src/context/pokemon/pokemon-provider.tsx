729da784a103a11c7485b51ed469143a18c55c3d
Hugo <hugo@boahstudio.com>
2023-06-19 20:03:07 -0300

    localStorage

--- src/context/pokemon/pokemon-provider.tsx
+++ src/context/pokemon/pokemon-provider.tsx
@@ -1,4 +1,4 @@
-import { Slot, component$, useContextProvider, useStore } from '@builder.io/qwik';
+import { Slot, component$, useContextProvider, useStore, useVisibleTask$ } from '@builder.io/qwik';
 
 import { PokemonGameContext, type PokemonGameState } from './pokemon-game.context';
 import { PokemonListContext, type PokemonListState } from './pokemon-list.context';
@@ -21,6 +21,26 @@ export const PokemonProvider = component$(() => {
     
     useContextProvider( PokemonGameContext, pokemomGame );
     useContextProvider( PokemonListContext, pokemomList );
+
+    useVisibleTask$(() => {
+        if ( localStorage.getItem('pokemon-game') ) {
+            const { 
+                isPokemonVisible = true,
+                pokemonId = 10,
+                showBackImage = false,
+            } = JSON.parse(localStorage.getItem('pokemon-game')!) as PokemonGameState;
+
+            pokemomGame.isPokemonVisible = isPokemonVisible;
+            pokemomGame.pokemonId = pokemonId;
+            pokemomGame.showBackImage = showBackImage;
+        }
+    });
+
+    useVisibleTask$(({ track }) => {
+        // se vuelve a ejecutar si algunas de las propiedades cambia
+        track(() => [ pokemomGame.isPokemonVisible, pokemomGame.pokemonId, pokemomGame.showBackImage ]);
+        localStorage.setItem('pokemon-game', JSON.stringify( pokemomGame ));
+    });
    
     return <Slot />;
 
