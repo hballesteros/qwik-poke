cf8d3e670bb0b269241ce59c667eba51655c4a75
Hugo <hugo@boahstudio.com>
2023-06-28 16:19:26 -0300

    manual form validation

---
+++ src/routes/dashboard/layout.tsx
@@ -0,0 +1,16 @@
+import { Slot, component$ } from '@builder.io/qwik';
+import Navbar from '~/components/shared/navbar/navbar';
+
+export default component$(() => {
+
+    return (
+        <>
+            <Navbar />
+            <div class="flex flex-col items-center justify-center mt-5">
+                <span class="text-5xl">Dashboard Layout</span>
+                <Slot />
+            </div>
+        </>
+    )
+
+})
